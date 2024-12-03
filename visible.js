2f4bd6fab738e0eb4abd8cc2c7b2f09b27057b7e
Adgar <YOUR EMAIL@SAMLECOM>
2024-12-03 20:16:12 +0300

    произвел небольшие доработки

--- visible.js
+++ visible.js
@@ -1,7 +1,8 @@
 let btnVisible  = document.querySelector('.catalog-nav__select_small');
 let formToFilter = document.querySelector('.modal-window');
 
-
 btnVisible.addEventListener('click', function() {
     formToFilter.classList.toggle('modal-window_unvisible')
     });
+
+
